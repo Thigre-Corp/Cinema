f2d44d56713fef0ab395b065ef398ab9f262e3ef
Thierry GRELL <thigre.sspi@gmail.com>
2025-06-27 17:04:18 +0200

    we

--- model/Connect.php
+++ model/Connect.php
@@ -5,7 +5,7 @@ namespace Model;
 abstract class Connect{
 
     const HOST = "localhost";
-    const DB = "cinema";
+    const DB = "cinema_thierry";
     const USER = "root";
     const PASS = "";
 
