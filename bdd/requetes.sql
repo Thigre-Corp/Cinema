fec77bcc2d992ff50e0573c38ec0be1695ab37ab
Thigre-Corp <thigre.sspi@gmail.com>
2025-06-29 09:38:40 +0200

    correction_requetes

--- bdd/requetes.sql
+++ bdd/requetes.sql
@@ -1,11 +1,11 @@
 --a. Informations d’un film (id_film) : titre, année, durée (au format HH:MM) et réalisateur
-SELECT f.film_titre, f.film_annee,  CONVERT(f.film_duree*60 AS TIME) AS duree , p.personne_nom, p.personne_prenom,
+SELECT f.film_titre, f.film_annee, CONCAT(f.film_duree DIV 60, 'H', f.film_duree MOD 60) AS Durée, p.personne_nom, p.personne_prenom
 FROM film f
 INNER JOIN realisateur r
 ON f.id_realisateur = r.id_realisateur
 LEFT JOIN personne p
-ON r.id_realisateur = p.id_personne
-WHERE id_film = {id_film}
+ON r.id_personne = p.id_personne
+WHERE f.id_film = 407;
 
 --b. Liste des films dont la durée excède 2h15 classés par durée (du + long au + court)
 SELECT film_titre
@@ -16,12 +16,10 @@ ORDER BY film_duree DESC
 --c. Liste des films d’un réalisateur (en précisant l’année de sortie)
 SELECT f.film_titre, f.film_annee
 FROM film f
-LEFT JOIN realisateur r
-WHERE r.id_realisateur = {id_realisateur}
-
+WHERE f.id_realisateur = 310
 
 --d. Nombre de films par genre (classés dans l’ordre décroissant)
-SELECT COUNT(f.film), g.genre_libelle
+SELECT COUNT(f.id_film), g.genre_libelle
 FROM film f
 INNER JOIN appartenir a
 ON f.id_film = a.id_film
@@ -32,7 +30,7 @@ GROUP BY g.genre_libelle
 --e. Nombre de films par réalisateur (classés dans l’ordre décroissant)
 SELECT COUNT(f.id_film), p.personne_nom
 FROM film f
-INNER JOIN relarisateur r 
+INNER JOIN realisateur r 
 ON f.id_realisateur = r.id_realisateur
 INNER JOIN personne p
 ON r.id_personne = p.id_personne
@@ -45,20 +43,17 @@ FROM personne p
 INNER JOIN acteur a
 ON p.id_personne = a.id_personne
 INNER JOIN casting c 
-ON a.id_personne = c.id_personne
-WHERE c.id_film = {id_film}
+ON a.id_acteur = c.id_acteur
+WHERE c.id_film = 404
 
 --g. Films tournés par un acteur en particulier (id_acteur) avec leur rôle et l’année de sortie (du film le plus récent au plus ancien)
-SELECT ro.role_nom, f.film_annee
-FROM role ro 
-INNER JOIN casting c 
-ON ro.id_role, c.id_role
-INNER JOIN acteur a 
-ON c.id_personne = a.id_personne
-INNER JOIN personne p 
-ON a.id_personne = p.id_personne
-INNER JOIN film f
-ON c.id_film = f.id_film
+SELECT f.film_titre , ro.role_nom, f.film_annee
+FROM film f
+INNER JOIN casting c
+ON f.id_film = c.id_film
+INNER JOIN role ro
+ON c.id_role = ro.id_role
+WHERE c.id_acteur = 12
 ORDER BY f.film_annee DESC
 
 --h. Liste des personnes qui sont à la fois acteurs et réalisateurs
@@ -71,9 +66,9 @@ ON r.id_personne = p.id_personne
 WHERE a.id_personne = r.id_personne
 
 --i. Liste des films qui ont moins de 5 ans (classés du plus récent au plus ancien)
-SELECT film_titre
+SELECT film_titre 
 FROM film
-WHERE DATE_FORMAT(film_annee, %Y) > SUBDATE(NOW()) - INTERVAL 5 YEAR)
+WHERE film_annee >  DATE_FORMAT(NOW(), "%Y") - 5
 ORDER BY film_annee DESC
 
 --j. Nombre d’hommes et de femmes parmi les acteurs
@@ -88,13 +83,14 @@ SELECT p.personne_nom, p.personne_prenom
 FROM personne p 
 RIGHT JOIN acteur a 
 ON p.id_personne = a.id_personne
-WHERE p.personne_dateNaissance >= SUBDATE(NOW()) - INTERVAL 50 YEAR)
+WHERE p.personne_dateNaissance <= SUBDATE(NOW(), INTERVAL 50 YEAR)
 
 --l. Acteurs ayant joué dans 3 films ou plus
 SELECT p.personne_nom, p.personne_prenom
 FROM personne p
 INNER JOIN acteur a 
-WHERE p.id_personne = a.id_personne
+ON p.id_personne = a.id_personne
 INNER JOIN casting c 
 ON a.id_acteur = c.id_acteur    
-WHERE count(c.id_film) >=3;
+GROUP BY p.personne_nom,  p.personne_prenom
+HAVING COUNT(DISTINCT c.id_film) >= 3
